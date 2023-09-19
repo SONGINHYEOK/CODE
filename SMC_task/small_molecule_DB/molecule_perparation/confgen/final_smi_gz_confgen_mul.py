15b7e3926a67085c7e1ca1d9af8faad432cbc1f1
inhyeoksong <sjfghy@naver.com>
2023-09-19 11:59:59 +0900

    edit final confgen

--- SMC_task/small_molecule_DB/molecule_perparation/confgen/final_smi_gz_confgen_mul.py
+++ SMC_task/small_molecule_DB/molecule_perparation/confgen/final_smi_gz_confgen_mul.py
@@ -1,61 +1,54 @@
-import gzip
-
-file_path = '/Users/song-inhyeog/CODEING/CODE/SMC_task/small_molecule_DB/molecule_perparation/confgen/H29P900.smi.gz'
-
-with gzip.open(file_path, 'rt') as gz_file:
-    content = gz_file.read().split('\n')
-    del content[-1]
-
-smiles_list = []
-for i in content:
-    smi = i.split(" ")[0].split("\t")[0]
-    #ori_id = i.split(" ")[0].split("\t")[1]
-    smiles_list.append(smi)
-
-#=====================================================================================================#
-
-import os
 import multiprocessing
 from rdkit import Chem
 from rdkit.Chem import AllChem
-from rdkit.Chem import SDWriter
 
-# Define a function to generate conformers for a single molecule
-def generate_conformers(molecule, num_conformers):
-    mol = Chem.MolFromSmiles(molecule)
-    if mol is None:
-        return None
-    AllChem.EmbedMultipleConfs(mol, numConformers=num_conformers)
-    return mol
+# Function to generate conformers for a molecule
+def generate_conformers(smiles, num_conformers, lock):
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is not None:
+        AllChem.EmbedMultipleConfs(mol, numConfs=num_conformers)
+        for conf_id in range(mol.GetNumConformers()):
+            AllChem.MMFFOptimizeMolecule(mol, confId=conf_id)
+        
+        with lock:
+            writer = Chem.SDWriter("conformers.sdf")
+            
+            for conf in mol.GetConformers():
+                mol.SetProp('ID', f'{smiles}_{conf.GetId()}')
+                writer.write(mol, confId=conf.GetId())
+            writer.close()
 
-# Define a function to process a batch of molecules in parallel
-def process_batch(batch, num_conformers, writer):
-    for molecule in batch:
-        conformer = generate_conformers(molecule, num_conformers)
-        if conformer:
-            writer.write(conformer)  # Write the conformer to the SDF file
+def main():
+    num_conformers = 10  # Number of conformers to generate
+    num_processes = 4 #multiprocessing.cpu_count()  # Number of CPU cores
 
-if __name__ == '__main__':
-    # Define your list of SMILES strings for the molecules you want to generate conformers for
-    
+    # Create a manager and a shared lock
+    with multiprocessing.Manager() as manager:
+        lock = manager.Lock()
+        
+        import gzip
 
-    # Specify the number of conformers to generate for each molecule
-    num_conformers_per_molecule = 20
+        file_path = '/Users/song-inhyeog/CODEING/CODE/SMC_task/small_molecule_DB/molecule_perparation/confgen/H29P900.smi.gz'
 
-    # Create an SDF writer to save the conformers
-    sdf_filename = 'conformers.sdf'
-    writer = SDWriter(sdf_filename)
+        with gzip.open(file_path, 'rt') as gz_file:
+            content = gz_file.read().split('\n')
+            del content[-1]
 
-    # Split the list of SMILES into batches for parallel processing
-    num_processes = 4#multiprocessing.cpu_count()
-    batch_size = len(smiles_list) // num_processes
-    batches = [smiles_list[i:i+batch_size] for i in range(0, len(smiles_list), batch_size)]
+        smiles_list = []
+        for i in content:
+            smi = i.split(" ")[0].split("\t")[0]
+            #ori_id = i.split(" ")[0].split("\t")[1]
+            smiles_list.append(smi)
+        
+        
+        # Split the conformer generation tasks across processes
+        pool = multiprocessing.Pool(processes=num_processes)
+        tasks = [(smiles, num_conformers, lock) for smiles in smiles_list]
+        pool.starmap(generate_conformers, tasks)
+        pool.close()
+        pool.join()
+        
+        print(f"Conformers generated successfully for {len(smiles_list)} molecules.")
 
-    # Create a multiprocessing pool to generate conformers in parallel
-    with multiprocessing.Pool(processes=num_processes) as pool:
-        pool.starmap(process_batch, [(batch, num_conformers_per_molecule, writer) for batch in batches])
-
-    # Close the SDF writer
-    writer.close()
-
-    print(f'Conformers saved to {sdf_filename}')
+if __name__ == "__main__":
+    main()
