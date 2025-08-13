1305e80353362b82e0629ff86d7be832a7d3e2fa
SONGINHYEOK <41423617+SONGINHYEOK@users.noreply.github.com>
2025-08-13 09:14:44 +0900

    Delete OMNIDRUG-AI directory

--- OMNIDRUG-AI/backend/apps/molecules/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Molecule
-
-@admin.register(Molecule)
-class MoleculeAdmin(admin.ModelAdmin):
-    list_display = ['id', 'smiles', 'molecular_weight', 'logp', 'source']
-    search_fields = ['smiles', 'canonical_smiles', 'inchi_key']
-    list_filter = ['source']
